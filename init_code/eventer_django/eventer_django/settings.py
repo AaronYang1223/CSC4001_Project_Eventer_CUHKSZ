0ec507b1288766df459e8550beec80679b549786
ylin <17835344407@163.com>
2022-03-24 17:40:30 +0800

    add user model

--- init_code/eventer_django/eventer_django/settings.py
+++ init_code/eventer_django/eventer_django/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'user',
+    # for picture
+    'imagekit'
+    
 ]
 
 MIDDLEWARE = [
---
+++ init_code/eventer_django/user/models.py
@@ -0,0 +1,31 @@
+from distutils.command.upload import upload
+from statistics import mode
+from time import timezone
+from xml.sax import default_parser_list
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+import uuid
+
+
+default_user_path = 'media/user/'
+
+def user_dictory(instance, filename):
+    filename  = 'media/user/' + '{}.png'.format(uuid.uuid4())
+    print(filename)
+    return filename
+
+# Create your models here.
+class User(models.Model):
+    
+    # django default 
+    # id = models.AutoField(primary_key = True)
+    email = models.EmailField()
+    first_name = models.CharField(max_length = 256)
+    last_name = models.CharField(max_length = 256)
+    nick_name = models.CharField(max_length = 256)
+    is_orginazation = models.BooleanField(default = False)
+    
+    # default UTC time in settings.py
+    create_time = models.DateTimeField(auto_now_add = True)
+    picture = ProcessedImageField(upload_to = user_dictory, default = default_user_path + 'default.png', processors = [ResizeToFill(100, 100)])
