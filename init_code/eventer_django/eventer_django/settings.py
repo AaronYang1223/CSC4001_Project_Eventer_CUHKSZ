9786d42207a94b2479614e60e86929d7b880cb6b
Amehi <71913275+Amehi@users.noreply.github.com>
2022-03-25 15:41:26 +0800

    init post module
    1. int post app
    2. modified eventer_django\setttings;
    3. modified eventer_django\urls;

--- init_code/eventer_django/eventer_django/settings.py
+++ init_code/eventer_django/eventer_django/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'rest_framework',
+
+    'post',
+
     'user',
     'activity',
     'comment_activity',
@@ -46,7 +49,7 @@ INSTALLED_APPS = [
     'imagekit',
     
     # fro richtext
-    'ckeditor'
+    'ckeditor',
     
 ]
 
