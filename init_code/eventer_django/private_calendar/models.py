ec62df1e710a093e9a29a2c747991a7fc77cad4a
ylin <17835344407@163.com>
2022-03-26 23:42:41 +0800

    update calendar model, add title for showing

--- init_code/eventer_django/private_calendar/models.py
+++ init_code/eventer_django/private_calendar/models.py
@@ -5,5 +5,9 @@ class Private_calendar(models.Model):
     
     activity_id = models.ForeignKey('activity.Activity', on_delete = models.CASCADE)
     user_id = models.ForeignKey('user.User', on_delete = models.CASCADE)
+    
+    # show calendar
+    activity_title = models.CharField(max_length = 256, default = 'None')
+    
     activity_date = models.DateField(auto_now = True)
     is_detele = models.BooleanField(default = False)
--- init_code/eventer_django/public_calendar/models.py
+++ init_code/eventer_django/public_calendar/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Public_calendar(models.Model):
     
     activity_id = models.ForeignKey('activity.Activity', on_delete = models.CASCADE)
+    
+    # show calendar
+    activity_title = models.CharField(max_length = 256, default = 'None')
     activity_date = models.DateField(auto_now = True)
     is_detele = models.BooleanField(default = False)
