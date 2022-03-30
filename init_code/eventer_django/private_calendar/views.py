9530f50c1a6dcd43797b0d67329e08dbea55a05a
Amehi <71913275+Amehi@users.noreply.github.com>
2022-03-30 21:17:53 +0800

    add post function
    app:
    private calendar;
    public calendar;

--- init_code/eventer_django/private_calendar/views.py
+++ init_code/eventer_django/private_calendar/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework import status
 from django.http import HttpResponse, JsonResponse
 from .serializers import Private_calendar_serializer
 from .models import Private_calendar
 import datetime
+
+
+def create_calendar_event(request):
+    if(request.method=='POST'):
+        serializer=Private_calendar_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def calendar(request, user, start_date, end_date):
     
--- init_code/eventer_django/public_calendar/urls.py
+++ init_code/eventer_django/public_calendar/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
 # date should be xxxx-xx-xx
 urlpatterns = [
-    path('public_calendar/<str:start_date>/<str:end_date>', views.calendar)
+    path('public_calendar/<str:start_date>/<str:end_date>', views.calendar),
+    path('public_calendar/create', views.create_calendar_event)
 ]
