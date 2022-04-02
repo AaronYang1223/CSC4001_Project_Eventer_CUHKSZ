f12c6b14e13920a7edd70a7ff8a47687fceaf5c7
ylin <17835344407@163.com>
2022-04-02 10:57:10 +0800

    add score post

--- init_code/eventer_django/score_activity/serializers.py
+++ init_code/eventer_django/score_activity/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Participant_Activity
+from .models import Participant_Activity, Score
 
 class Participant_Activity_serializer(serializers.ModelSerializer):
     class Meta:
         model = Participant_Activity
         fields = ['id', 'user_id__first_name', 'user_id__last_name', 'user_id__email']
+
+class Score_activity_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Score
+        fields = ['id', 'user_id', 'activity_id', 'score']
--- init_code/eventer_django/score_activity/views.py
+++ init_code/eventer_django/score_activity/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
-from .serializers import Participant_Activity_serializer
-from .models import Participant_Activity
+from .serializers import Participant_Activity_serializer, Score_activity_serializer
+from .models import Participant_Activity, Score
+from activity.models import Activity
 
 @csrf_exempt
 def participant_activity(request, activity_id):
@@ -19,3 +20,42 @@ def participant_activity(request, activity_id):
             return JsonResponse(participant_activity_list2, json_dumps_params = {'ensure_ascii': False}, safe = False)
         except:
             return HttpResponse(status = 404)
+
+@csrf_exempt
+def participant_activity_add(request):
+    
+    if (request.method == 'POST'):
+        data = JSONParser().parse(request)
+        serializers = Participant_Activity_serializer(data = data)
+        if (serializers.is_valid()):
+            serializers.save()
+            return JsonResponse(serializers.data, status = 201)
+        return JsonResponse(serializers.errors, status = 400)
+    
+@csrf_exempt
+def score_add(request):
+    if (request.method == 'POST'):
+        data = JSONParser().parse(request)
+        serializers = Score_activity_serializer(data = data)
+        if (serializers.is_valid()):
+            
+            # check if score exists
+            scores = Score.objects.filter(user_id = data['user_id'], activity_id = data['activity_id'])
+            if (scores.values()):
+                return JsonResponse('score already exist: user_id: {}, activity_id: {}, score: {}'.format(data['user_id'], data['activity_id'], data['score']), status = 400, safe = False)
+        
+            # update activity score
+            activity = Activity.objects.get(id = data['activity_id'])
+            
+            # check if activity is finished
+            if (not activity.is_outdate):
+                return JsonResponse('activity {} is not finished'.format(data['activity_id']), status = 400, safe = False)
+            
+            # update activity score
+            activity.score_avg = (activity.score_avg * activity.score_num + data['score']) / (activity.score_num + 1)
+            activity.score_num += 1
+            activity.save()
+            serializers.save()
+            
+            return JsonResponse(serializers.data, status = 201)
+        return JsonResponse(serializers.errors, status = 400)
