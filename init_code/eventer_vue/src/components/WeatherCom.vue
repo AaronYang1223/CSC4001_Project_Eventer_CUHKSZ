b13c5d969517b94109222b3ac44481db2d767090
ylin <17835344407@163.com>
2022-04-12 02:14:59 +0800

    add weather api

--- init_code/eventer_vue/src/components/WeatherCom.vue
+++ init_code/eventer_vue/src/components/WeatherCom.vue
@@ -20,7 +20,7 @@
           <v-list-item two-line>
             <v-list-item-content>
               <v-list-item-title >
-                <h1 class="font-weight-black">CUHKSZ</h1>       
+                <h1 class="font-weight-black">{{city}}</h1>       
               </v-list-item-title>
               <v-list-item-subtitle>{{date}}, {{time}}, {{weather}}</v-list-item-subtitle>
             </v-list-item-content>
@@ -39,7 +39,7 @@
           height="83"
         >
 
-          <h1 class="font-weight-thin text-center">23&deg;C</h1>
+          <h1 class="font-weight-thin text-center">{{temperature}}&deg;C</h1>
 
           <v-img
             :src="weather_img"
@@ -54,10 +54,24 @@
 <script>
   export default {
     data: () => ({
-      date: "Mon",
-      time: "12:30 PM",
+      date: "2022-04-12",
+      time: "02:00:29",
       weather: "Mostly sunny",
       weather_img: "https://cdn.vuetifyjs.com/images/cards/sun.png",
+      city: "Shenzhen",
     }),
+    mounted() {
+      this.weather_link = 'https://restapi.amap.com/v3/weather/weatherInfo?city=440307&key=4e5aad6906d160b50e6018ea4891b29c'
+      this.$axios.get(this.weather_link).then(response => {
+        this.temperature = response.data.lives[0].temperature
+        this.date = response.data['lives'][0]['reporttime'].split(' ')[0]
+        this.time = response.data['lives'][0]['reporttime'].split(' ')[1]
+        this.weather = response.data['lives'][0]['weather'],
+        this.city = response.data['lives'][0]['province'] + ' ' + response.data['lives'][0]['city']
+        this.humidity = response.data['lives'][0]['humidity']
+        this.winddirection = response.data['lives'][0]['winddirection']
+        this.windpower = response.data['lives'][0]['windpower']
+      })
+    }
   }
 </script>
