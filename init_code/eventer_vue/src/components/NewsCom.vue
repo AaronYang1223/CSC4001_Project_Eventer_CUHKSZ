dcf5bd42ec4aa453ee78ef89fde28adb055cbb8a
ylin <17835344407@163.com>
2022-04-11 23:56:55 +0800

    update hot post and event

--- init_code/eventer_vue/src/components/NewsCom.vue
+++ init_code/eventer_vue/src/components/NewsCom.vue
@@ -100,6 +100,53 @@
     },
     created(){
 
+      this.posts_link = 'http://127.0.0.1:8000/api/post/order/comment_number/5'
+      this.$axios.get(this.posts_link).then(response => {
+        this.posts = []
+        for (let i = 0; i < response.data.length; i++) {
+          this.posts.push(
+            {
+              post_id: response.data[i].id,
+              title: response.data[i].post_title,
+              content: response.data[i].post_content,
+              tags: response.data[i].post_tag.split(' '),
+              avatar: 'http://127.0.0.1:8000' + response.data[i].picture,
+              is_authenticated: response.data[i].is_organization,
+              nickname: response.data[i].nick_name,
+              comment_num: response.data[i].comment_number,
+            }
+          )
+        }
+      })
+
+      this.activity_link = 'http://127.0.0.1:8000/api/activity/order/comment_number/5'
+      this.$axios.get(this.activity_link).then(response => {
+        this.events = []
+        console.log(response.data)
+        for (let i = 0; i < response.data.length; i++) {
+          this.events.push(
+            {
+              activity_id: response.data[i].id,
+              banner: 'http://127.0.0.1:8000' + response.data[i].cover_page,
+              title: response.data[i].title,
+              content: response.data[i].content,
+              tags: response.data[i].tag.split(' '),
+              avatar: 'http://127.0.0.1:8000' + response.data[i].picture,
+              is_authenticated: response.data[i].is_organization,
+              nickname: response.data[i].nick_name,
+              upper_num: response.data[i].max_participant_num,
+              attend_num: response.data[i].participant_num,
+              start_time: response.data[i].start_time,
+              end_time: response.data[i].end_time,
+              selection: 0,
+              show: false,
+            }
+          )
+        }
+      })
+
+
+
     },
     data: () => ({
       posts: [
